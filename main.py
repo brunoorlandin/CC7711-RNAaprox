25a96ce927adba42148ec24e3a4c0aab1105472a
brunoorlandin <bruno.orlandin@hotmail.com>
2022-09-11 18:23:33 -0300

    dev

--- main.py
+++ main.py
@@ -7,53 +7,54 @@ arquivo = np.load('teste2.npy')
 x = arquivo[0]
 y = np.ravel(arquivo[1])
 
-list_min = []
+minErrors = []
 
-menor_erro = 0;
+data = {}
 
-for i in range(2):
-
-    regr = MLPRegressor(hidden_layer_sizes=(2),
-                        max_iter=1000,
+for i in range(5):
+    regr = MLPRegressor(hidden_layer_sizes=(80),
+                        max_iter=100000,
                         activation='logistic', #{'identity', 'logistic', 'tanh', 'relu'},
                         solver='adam',
                         learning_rate = 'adaptive',
                         n_iter_no_change=50)
-    print('Treinando RNA')
+    print('Treinando RNA %d' %(i+1))
     regr = regr.fit(x,y)
 
-
-    print('Preditor')
+    print('Preditor %d' %(i+1))
     y_est = regr.predict(x)
 
+    minErrors.append(regr.best_loss_)
+
+    minError = regr.best_loss_
+
+    data[minError] = [regr.loss_curve_.copy(), y_est.copy()]
+
+minError = min(data.keys())
+
+curve = data[minError][0]
+y_est = data[minError][1]
+
+plt.figure(figsize=[14,7])
+
+#plot curso original
+plt.subplot(1,3,1)
+plt.plot(x,y)
+
+#plot aprendizagem
+plt.subplot(1,3,2)
+plt.plot(curve)
+
+#plot regressor
+plt.subplot(1,3,3)
+plt.plot(x,y,linewidth=1,color='yellow')
+plt.plot(x,y_est,linewidth=2)
+
+plt.show()
 
 
-
-
-
-    plt.figure(figsize=[14,7])
-
-    #plot curso original
-    plt.subplot(1,3,1)
-    plt.plot(x,y)
-
-    #plot aprendizagem
-    plt.subplot(1,3,2)
-    plt.plot(regr.loss_curve_)
-    list_min.append(regr.best_loss_)
-
-    
-    print(regr.best_loss_)
-
-    #plot regressor
-    plt.subplot(1,3,3)
-    plt.plot(x,y,linewidth=1,color='yellow')
-    plt.plot(x,y_est,linewidth=2)
-
-    plt.show()
-
-
-
-
-print("Média:" + str(round(np.average(list_min),2)))
-print("Desvio padrão:" + str(round(np.std(list_min),2)))
+# print(minErrors)
+# print(data.keys())
+# print(min(data.keys()))
+print("Média do erro: " + str(round(np.average(minErrors),2)))
+print("Desvio padrão do erro: " + str(round(np.std(minErrors),5)))
